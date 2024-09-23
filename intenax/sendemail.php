10eb2d1a585a259f9b48ac4e218c952f788f3753
vaguebound <mcshifan@gmail.com>
2024-09-23 14:30:10 +0500

    files added

---
+++ intenax/sendemail.php
@@ -0,0 +1,32 @@
+<?php
+
+// Define some constants
+define( "RECIPIENT_NAME", "John Doe" );
+define( "RECIPIENT_EMAIL", "youremail@mail.com" );
+
+
+// Read the form values
+$success = false;
+$userName = isset( $_POST['name'] ) ? preg_replace( "/[^\s\S\.\-\_\@a-zA-Z0-9]/", "", $_POST['name'] ) : "";
+$senderEmail = isset( $_POST['email'] ) ? preg_replace( "/[^\.\-\_\@a-zA-Z0-9]/", "", $_POST['email'] ) : "";
+$userPhone = isset( $_POST['phone'] ) ? preg_replace( "/[^\s\S\.\-\_\@a-zA-Z0-9]/", "", $_POST['phone'] ) : "";
+$senderServices = isset( $_POST['services'] ) ? preg_replace( "/[^\.\-\_\@a-zA-Z0-9]/", "", $_POST['services'] ) : "";
+$message = isset( $_POST['message'] ) ? preg_replace( "/(From:|To:|BCC:|CC:|Subject:|Content-Type:)/", "", $_POST['message'] ) : "";
+
+// If all values exist, send the email
+if ( $userName && $senderEmail && $userPhone && $senderServices && $message) {
+  $recipient = RECIPIENT_NAME . " <" . RECIPIENT_EMAIL . ">";
+  $headers = "From: " . $userName . "";
+  $msgBody = " Email: ". $senderEmail . " Phone: ". $userPhone . " Services: ". $senderServices . " Message: " . $message . "";
+  $success = mail( $recipient, $headers, $msgBody );
+
+  //Set Location After Successsfull Submission
+  header('Location: index.html?message=Successfull');
+}
+
+else{
+	//Set Location After Unsuccesssfull Submission
+  	header('Location: contact.html?message=Failed');	
+}
+
+?>
