61e037c62351c7ec18bde903cd9687def295f8f3
vaguebound <mcshifan@gmail.com>
2024-10-17 16:04:39 +0500

    header

--- intenax/js/main-slider-script.js
+++ intenax/js/main-slider-script.js
@@ -88,7 +88,7 @@
                 jsFileLocation:"plugins/revolution/js/",
                 sliderLayout:"fullwidth",
                 dottedOverlay:"none",
-                delay:10000,
+                delay:5000,
                 navigation: {
                     keyboardNavigation:"off",
                     keyboard_direction: "horizontal",
